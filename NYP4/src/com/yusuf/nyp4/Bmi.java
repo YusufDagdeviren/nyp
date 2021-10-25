f3fc9815c0c2ff55dd65bb8e7c241710addae312
YazilimOfisim <87547919+xSentez@users.noreply.github.com>
2021-10-25 14:42:49 +0300

    Update Bmi.java

--- NYP4/src/com/yusuf/nyp4/Bmi.java
+++ NYP4/src/com/yusuf/nyp4/Bmi.java
@@ -10,4 +10,47 @@ package com.yusuf.nyp4;
  */
 public class Bmi {
     
+    private String name;
+    private int age;
+    private double weight; 
+    private double height;
+
+    
+    /*
+    ağırlık / boy^2
+    
+    */
+    public Bmi(int age, double weight, double height) {
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public Bmi(String name, double weight, double height) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+    
+    public double getBmi(){
+        
+        return weight / Math.pow(height,2);
+        
+        
+    }
+    public String getStatus(){
+        if(getBmi()<18.49){
+            return "Çok zayıf";
+        }
+        else if(18.49<getBmi() && getBmi()<24.99){
+            return "İdeal";
+        }
+        else if(25<getBmi() && getBmi()<29.99){
+            return "Kilolu";
+        }
+        else if(getBmi()>30){
+            return "Aşrırı kilolu";
+        }
+    }
+    
 }

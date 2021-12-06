755a77c922ffb86031bcbf201532f674d6078b9a
YazilimOfisim <87547919+xSentez@users.noreply.github.com>
2021-12-06 10:42:22 +0300

    nyp10

---
+++ nyp10/src/com/yusuf/nyp2/Circle.java
@@ -0,0 +1,45 @@
+package com.yusuf.nyp2;
+
+public class Circle implements Comparable<Circle>{
+
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+
+        return Math.PI * Math.pow(radius,2);
+    }
+
+    public double getPerimeter(){
+        return 2*Math.PI*radius;
+    }
+
+
+
+
+    @Override
+    public int compareTo(Circle o) {
+        if(getArea()>o.getArea()){
+            return 1;
+        }
+
+        else if(getArea()==o.getArea()){
+            return 0;
+        }
+        else{
+            return -1;
+        }
+
+    }
+}
---
+++ nyp10/src/com/yusuf/nyp2/Main.java
@@ -0,0 +1,15 @@
+package com.yusuf.nyp2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Circle circle1 = new Circle(4);
+        System.out.println(circle.compareTo(circle1));
+
+
+
+    }
+
+
+}
